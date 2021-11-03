d680db2d7456ed23c6d56e3aa2f707c59b8d6587
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2021-11-03 17:29:39 +0000

    daily commits

--- day3.js
+++ day3.js
@@ -27,7 +27,7 @@ const alarm = {
     } else {
       console.log(
         `I don't recognise "${day}".` +
-          `Please write in the style "mon", "tue" etc.`
+        `Please write in the style "mon", "tue" etc.`
       );
       return;
     }
@@ -38,70 +38,3 @@ const alarm = {
 // alarm.setDay("sat");
 // alarm.setDay("glorp");
 
-// PET
-const Pet = function (name, type, age, colour) {
-  this.name = name;
-  this.typeOfPet = type;
-  this.age = age;
-  this.colour = colour;
-  this.nourished = 20;
-  this.health = 100;
-
-  //Prints a dividing line
-  this.bar = () => {
-    console.log("\n_____\n");
-  };
-
-  //Prints pet's stats in full
-  this.print = () => {
-    this.bar();
-    console.log(`Your pet's stats:`);
-    for (const key in this) {
-      if (typeof this[key] != "function") {
-        console.log(`${key}: ${this[key]}`);
-      }
-    }
-  };
-
-  this.foodFeels = () => {
-    if (this.nourished <= 0) {
-      this.health = -5;
-    }
-    if (this.nourished < 10) {
-      feels.push("is starving");
-    }
-    if (this.nourished > 30) {
-      feels.push("has overeaten");
-    }
-    if (this.nourished > 35) {
-      this.digest(-10, "vomit");
-    }
-    return feels;
-  };
-
-  //processes all internal statrs and outputs how pet is feeling
-  this.feels = () => {
-    let feels = ["test", "test2"];
-    feels = this.foodFeels(feels);
-    for (const feel in feels) {
-      console.log(feel);
-    }
-  };
-
-  // Changes self.nourished and writes description of action to log.
-  this.digest = (nutrition, verb) => {
-    this.bar();
-    this.nourished += nutrition;
-    console.log(`${this.name} is ${verb}ing!`);
-    this.feels();
-    // this.print();
-  };
-};
-
-// birth Wiggles
-const wiggles = new Pet("Wiggles", "pig", 21, "eau-de-nil");
-
-// demo of eat/drink/other function
-wiggles.digest(5, "eat");
-wiggles.digest(1, "drink");
-wiggles.digest(31, "eat");
--- day_1.js
+++ day_1.js
@@ -1,5 +1,81 @@
 function hey_world() {    
-    console.log("Hiya globe.")
+    console.log("Hiya globe.");
 }
 
-hey_world()
+// hey_world()
+
+function demonstrateStringMethods() {
+    let myString = "some lettters in a pattern";
+    myString = myString.toUpperCase();
+    console.log(myString);
+    myString = myString.lastIndexOf("E");
+    console.log(myString);
+}
+
+// demonstrateStringMethods();
+
+function demonstrateRandom(){
+    console.log(Math.floor(Math.random()*10))
+}
+
+// demonstrateRandom();
+
+function printGrid(){
+    let bars = "    |   |   ";
+    let rows = "-------------";
+    console.log(bars);
+    console.log(bars);
+    console.log(rows);
+    console.log(bars);
+    console.log(bars);
+    console.log(rows);
+    console.log(bars);
+    console.log(bars);
+}
+
+// printGrid()
+
+function print8thCharacter(){
+    let myString = "All around the world"
+    myString = myString.charAt(7)
+    console.log(myString.toUpperCase()) 
+}
+
+// print8thCharacter()
+
+function declareFaves(){
+    let favColour = "Teal";
+    let name = "Ben";
+    let age = "34";
+
+    myString = `My name is ${name}, I am ${age} years old. My favourite colour is ${favColour}`;
+    console.log(myString)
+}
+
+// declareFaves()
+
+function nameMeals (){
+    
+    function printMeals(meals){
+        console.log(
+            `For breakfast I'll have ${meals.breakfast}, ` +
+            `for lunch I'll eat ${meals.lunch} and then for dinner ` +
+            `I'll have ${meals.dinner}`);    
+    }
+    
+    let meals ={
+        "breakfast" : "eggs",
+        "lunch" : "eggs",
+        "dinner" : "eggs"
+    };
+       
+    printMeals(meals);
+    meals.breakfast = "jam"
+    printMeals(meals)
+}
+
+// nameMeals();
+
+function howOldInDays(){
+    
+}
