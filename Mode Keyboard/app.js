76b4548bd1690c61c9a319f46c4ab333fa9bb32e
BenjaminGordonCody <b.gordoncody@gmail.com>
2021-11-16 17:02:52 +0000

    keys play, keys light up,

--- Mode Keyboard/app.js
+++ Mode Keyboard/app.js
@@ -1,5 +1,44 @@
+//define audio context
 const AudioContext = window.AudioContext;
 const audioContext = new AudioContext();
+
+// make divs for keyboard keys
+const makeDivKeys = (parentDivID, idPrefix) => {
+  parent = document.getElementById(parentDivID);
+
+  for (let i = 0; i < 8; i++) {
+    let newdiv = document.createElement("div");
+    newdiv.id = `${idPrefix}${i}`;
+    newdiv.className = "pianoKey";
+    parent.appendChild(newdiv);
+  }
+};
+
+makeDivKeys("toprow", "top");
+makeDivKeys("bottomrow", "bottom");
+
+//put divs in array for easy referencing
+let divArray = document.getElementsByClassName("pianoKey");
+
+//array of colours to match the divs above
+let colourArray = [
+  "red",
+  "yellow",
+  "blue",
+  "green",
+  "purple",
+  "cornflowerBlue",
+  "darkOrchid",
+  "darkSeaGreen",
+  "yellow",
+  "blue",
+  "green",
+  "purple",
+  "cornflowerBlue",
+  "darkOrchid",
+  "darkSeaGreen",
+  "red",
+];
 
 const keymap = {
   q: 0,
@@ -10,9 +49,18 @@ const keymap = {
   y: 5,
   u: 6,
   i: 7,
+  a: 8,
+  s: 9,
+  d: 10,
+  f: 11,
+  g: 12,
+  h: 13,
+  j: 14,
+  k: 15,
 };
 
 let piano = {
+  keys: [],
   _getKey: function (frequency) {
     // define new oscillator
     let oscillator = audioContext.createOscillator();
@@ -38,25 +86,32 @@ let piano = {
   init: function () {
     // piano.keys = piano._getKey(229);
     // console.log(piano.keys);
+
+    for (let index = 0; index < 16; index++) {
+      piano.keys[index] = piano._getKey(300 * (index + 1));
+    }
   },
 };
 
 let startButton = document.createElement("button");
 startButton.addEventListener("click", piano.init);
-document.body.append(startButton);
+document.body.appendChild(startButton);
 
-window.addEventListener("keydown", () => {
-  piano.keys.gain.gain.value = 1;
+window.addEventListener("keydown", (event) => {
+  key = keymap[event.key];
+  piano.keys[key].gain.gain.value = 1;
+  divArray[key].style.backgroundColor = colourArray[key];
 });
 
-window.addEventListener("keyup", () => {
-  piano.keys.gain.gain.value = 0;
+window.addEventListener("keyup", (event) => {
+  key = keymap[event.key];
+  piano.keys[key].gain.gain.value = 0;
+  divArray[key].style.backgroundColor = "white";
 });
 
 // // TODO
 // generate keys in for loop, major only
-// play keys using keymap
-// add reverb
+
 // style css into grid
 // have grid respond to keypress (maybe make array of divs?)
 // can i be modal?
