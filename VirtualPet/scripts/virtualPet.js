8f1030f0747570aa1c2000e61708d5d69ee8a09f
BenjaminGordonCody <b.gordoncody@gmail.com>
2021-11-24 12:30:31 +0000

    formatted by prettier

--- VirtualPet/scripts/virtualPet.js
+++ VirtualPet/scripts/virtualPet.js
@@ -10,7 +10,6 @@ TODO
 - implement death.
 */
 
-
 // Shorthand functions to simplify writing
 const print = (thing, id) => {
   document.getElementById(id).innerHTML = thing;
@@ -38,13 +37,13 @@ const getPara = (string) => {
 };
 
 // Stat boundaries + related numbers
-let health ={
+let health = {
   max: 100,
   high: 75,
   low: 25,
   starvationPenalty: -10,
-  despairPenalty: -5
-}
+  despairPenalty: -5,
+};
 
 let happy = {
   max: 100,
@@ -54,9 +53,8 @@ let happy = {
   starvationPenalty: -5,
   fullnessBonus: 6,
   sicknessPenalty: -5,
-  healthBonus: 7
-  
-}
+  healthBonus: 7,
+};
 
 let nourished = {
   max: 100, // nourished can exceed max, but this causes illness
@@ -66,14 +64,13 @@ let nourished = {
   vomit: -10,
   food: 15,
   drink: 6,
-}
+};
 
 let educated = {
   max: 100,
   high: 75,
-  low:25
-}
-
+  low: 25,
+};
 
 // Pet constructor, also contains all pet methods
 const Pet = function (name, type, age, colour) {
@@ -95,10 +92,7 @@ const Pet = function (name, type, age, colour) {
   statement and causes the knock on effects if a stat crosses a threshold
   Once all emotional statements are collected, they are printed to page.*/
   this.output = () => {
-    output = [
-      this.getDigestiveFeels, 
-      this.getHappyFeels,
-      this.getHealthFeels];
+    output = [this.getDigestiveFeels, this.getHappyFeels, this.getHealthFeels];
 
     for (let item in output) {
       output[item] = `${this.name} ${output[item]()}`;
@@ -106,7 +100,7 @@ const Pet = function (name, type, age, colour) {
       append(output[item], "text");
     }
   };
-  
+
   //Prints pet's stats in full
   this.stats = () => {
     clear("stats");
@@ -121,59 +115,53 @@ const Pet = function (name, type, age, colour) {
 
   /// STAT SETTERS
   // Thse methods alter the pet's stats, but stop them crossing max/min values
-  this.changeHappy = (num) =>{
+  this.changeHappy = (num) => {
     tempHappy = this.happy + num;
-    if (tempHappy > happy.max){
+    if (tempHappy > happy.max) {
       this.happy = happy.max;
-    } 
-    else if (tempHappy < happy.min) {
+    } else if (tempHappy < happy.min) {
       this.happy = happy.min;
-    }
-    else{
+    } else {
       this.happy = tempHappy;
     }
   };
 
-  this.changeHealth = (num) =>{
+  this.changeHealth = (num) => {
     tempHealth = this.health + num;
-    if (tempHealth > health.max){
+    if (tempHealth > health.max) {
       this.health = health.max;
-    } 
+    }
     if (tempHealth < health.min) {
       this.health = health.min;
-    }
-    else{
+    } else {
       this.health = tempHealth;
     }
-  }
+  };
 
-  this.changeNourished = (num) =>{
+  this.changeNourished = (num) => {
     tempNourished = this.nourished + num;
-    if (tempNourished > nourished.max){
+    if (tempNourished > nourished.max) {
       this.nourished = nourished.max;
     }
-    if (tempNourished < nourished.min){
+    if (tempNourished < nourished.min) {
       this.nourished = nourished.min;
-    }
-    else{
+    } else {
       this.nourished = tempNourished;
     }
-  }
-
-
+  };
 
   /// FEELING GETTERS
   // These functions check what a particular stat is, enact any related
   // consequences, and return a string describing the pet's state.
   this.getHealthFeels = () => {
-    if (this.health < health.low/2) {
-      this.changeHappy(happy.sicknessPenalty)
+    if (this.health < health.low / 2) {
+      this.changeHappy(happy.sicknessPenalty);
       return "is dying.";
     } else if (this.health <= health.low) {
-      this.changeHappy(happy.sicknessPenalty)
+      this.changeHappy(happy.sicknessPenalty);
       return "isn't well.";
     } else if (this.health >= health.high) {
-      this.changeHappy(happy.healthBonus)
+      this.changeHappy(happy.healthBonus);
       return "is physically spectacular";
     }
     return "is physically adequate";
@@ -181,14 +169,14 @@ const Pet = function (name, type, age, colour) {
 
   this.getDigestiveFeels = () => {
     if (this.nourished <= 0) {
-      this.changeHealth(health.starvationPenalty)
+      this.changeHealth(health.starvationPenalty);
       this.changeHappy(happy.starvationPenalty);
       return "is starving.";
     }
     if (this.nourished >= nourished.high) {
       this.changeHealth(this.nourished - nourished.high);
       if (this.nourished >= nourished.max) {
-        this.nourished -= nourished.vomit; 
+        this.nourished -= nourished.vomit;
         return "is vomiting."; //this.digest not used as causes loop
       } else {
         return "has overeaten";
@@ -202,25 +190,21 @@ const Pet = function (name, type, age, colour) {
     }
   };
 
-  this.getHappyFeels=()=>{
+  this.getHappyFeels = () => {
     if (this.happy == happy.max) {
       return "couldn't be happier";
-    }
-    else if (this.happy >= happy.high){
+    } else if (this.happy >= happy.high) {
       return "is very happy";
-    }
-    else if (this.happy >= happy.low){
+    } else if (this.happy >= happy.low) {
       return "is happy";
-    }
-    else if (this.happy < 0){
+    } else if (this.happy > 0) {
       return "is unhappy";
-    }
-    else{
+    } else {
       this.changeHealth(health.despairPenalty);
       return "is in despair";
     }
   };
-/// ACTIONS THE PET CAN DO
+  /// ACTIONS THE PET CAN DO
   this.digest = (nutrition, verb) => {
     clear("text");
     this.changeNourished(nutrition);
